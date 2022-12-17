d18509243fa34be9cc80399a6109358a46d45151
GHOPsT <mggsalvator@gmail.com>
2022-12-17 09:09:54 -0500

    se añadio un archivo para probar el algoritmo

---
+++ Prueba_algoritmo.py
@@ -0,0 +1,109 @@
+#Matrices
+
+ListaBebida = ["1","2","3","4","5","24","12","6","7","8","9","10","23","11"]
+#MatrizPrincipalProductosBebida = [
+#    [1,1,0,0,0,0],
+#    [1,1,0,0,0,0],
+#    [1,1,0,0,0,0],
+#    [1,1,0,0,0,0],
+#    [1,0,1,0,0,0],
+#    [1,0,1,0,0,0],
+#    [1,0,0,1,0,0],
+#    [0,1,0,0,1,0],
+#    [0,1,0,0,1,0],
+#    [0,1,0,0,1,0],
+#    [0,0,0,0,1,1],
+#    [0,0,0,1,1,0],
+#    [0,0,0,0,1,1],
+#    [0,0,0,1,1,0]
+#   ]
+
+MatrizPrincipalProductosBebida = [
+    [1,1,1,0,0,0,0],
+    [2,1,1,0,0,0,0],
+    [3,1,1,0,0,0,0],
+    [4,1,1,0,0,0,0],
+    [5,1,0,1,0,0,0],
+    [24,1,0,1,0,0,0],
+    [12,1,0,0,1,0,0],
+    [6,0,1,0,0,1,0],
+    [7,0,1,0,0,1,0],
+    [8,0,1,0,0,1,0],
+    [9,0,0,0,0,1,1],
+    [10,0,0,0,1,1,0],
+    [23,0,0,0,0,1,1],
+    [11,0,0,0,1,1,0]
+   ]
+
+ListaComida = ["13","14","15","16","17","18","19","20","21","22"]
+#MatrizPrincipalProductosComida = [
+#    [1,1,0,0,0,0],
+#    [1,0,1,0,0,0],
+#    [1,1,0,0,0,0],
+#    [0,0,1,1,0,0],
+#    [0,1,0,1,0,0],
+#    [0,0,1,1,0,0],
+#    [0,1,0,1,0,0],
+#    [0,0,0,1,1,0],
+#    [0,0,0,0,1,1],
+#    [0,1,0,0,1,0]
+#    ]
+
+MatrizPrincipalProductosComida = [
+    [13,1,1,0,0,0,0],
+    [14,1,0,1,0,0,0],
+    [15,1,1,0,0,0,0],
+    [16,0,0,1,1,0,0],
+    [17,0,1,0,1,0,0],
+    [18,0,0,1,1,0,0],
+    [19,0,1,0,1,0,0],
+    [20,0,0,0,1,1,0],
+    [21,0,0,0,0,1,1],
+    [22,0,1,0,0,1,0]
+    ]
+Orden = []
+Pedidos = []
+
+
+#Se visualiza los productos para comprar
+print("Bienvenido a Coffee Peluche")
+print("Bebida")
+MPB = len(MatrizPrincipalProductosBebida)
+for i in range(MPB):
+    print(MatrizPrincipalProductosBebida[i][0], end=" ")
+    print()
+print("Comida")
+MPC = len(MatrizPrincipalProductosComida)
+for i in range(MPC):
+    print(MatrizPrincipalProductosComida[i][0], end=" ")
+    print()
+Pedidos = []
+
+#Se selecciona el pedido
+Orden = input("Digite su orden: ")
+
+#Numero de filas
+filas = 3
+#Numero de columnas
+columnas = 3
+
+#El pedido se guarda en una matriz
+#for i in MatrizPrincipalProductosBebida:
+   #if Orden == MatrizPrincipalProductosBebida[i][0]:
+for i in range(filas):
+    Pedidos.append([])
+    for j in range(columnas):
+        Pedidos[i].append(Orden)
+print(Pedidos)
+#for i in MatrizPrincipalProductosComida:
+#    if Orden == MatrizPrincipalProductosComida[i][0]:
+
+#Se califica los pedidos
+
+#Se multiplica lo pedido por la matriz de pedidos
+
+#Se suma por colummnas y se guarda en arreglo sumas
+
+#Se pondera las sumas
+
+
